79aab0b0255811538aab6839fe13eefcf8349b01
rudiath <rudiath.r@gmail.com>
2026-06-25 06:32:54 +0700

    add PWA offline support with online/offline toggle

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE = 'toko-yakin-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/dist/output.css',
+  '/favicon.ico',
+  '/manifest.json',
+  '/xlsx.full.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+
+  if (e.request.url.startsWith(self.location.origin) || url.pathname.endsWith('.xlsx')) {
+    e.respondWith(
+      fetch(e.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      }).catch(() => caches.match(e.request))
+    );
+    return;
+  }
+
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
